a427e6fedfa125b0b724e2f92b7675b3eda9a5a9
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-09-05 13:20:53 +0300

    Adjust Routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('pages.index');
+// });
+Route::view('/', 'pages.index')->name('home');
+Route::view('/about', 'pages.about')->name('about');
+Route::view('/shop', 'pages.shop')->name('shop');
+Route::view('/contact', 'pages.contact')->name('contact');
