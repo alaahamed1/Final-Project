9153fbee472bcaba3003ab0756022d34ed72dcf8
Levi <dragonball14499@gmail.com>
2024-09-18 16:30:05 +0300

    Revert mass delete

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,35 +2,53 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * Class Category
+ *
+ * @property $id
+ * @property $title
+ * @property $description
+ * @property $create_user_id
+ * @property $update_user_id
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @property User $user
+ * @mixin Builder
+ */
 class Category extends Model
 {
-    use HasFactory, SoftDeletes;
 
-    protected $guarded = [];
+    protected $perPage = 20;
 
     /**
-     * Get the user who created the category.
+     * The attributes that are mass assignable.
      *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     * @var array<int, string>
      */
-    public function create_user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    protected $fillable = ['title', 'description', 'create_user_id', 'update_user_id'];
+
+
+    /**
+     * @return BelongsTo
+     */
+    public function createdByUser()
     {
         return $this->belongsTo(User::class, 'create_user_id', 'id');
     }
 
     /**
-     * Get the user who updated the category.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     * @return BelongsTo
      */
-    public function update_user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    public function updatedByUser()
     {
         return $this->belongsTo(User::class, 'update_user_id', 'id');
     }
+
     /**
      * Get the subcategories for the category.
      *
@@ -40,4 +58,5 @@ class Category extends Model
     {
         return $this->hasMany(SubCategory::class);
     }
+
 }
