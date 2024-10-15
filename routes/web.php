fcdd746a05b54ba24e157639bbdd70070bfe5c3c
Levi <dragonball14499@gmail.com>
2024-10-15 16:08:01 +0300

    Cleanup routes web.php

--- routes/web.php
+++ routes/web.php
@@ -1,68 +1,63 @@
 <?php
 
 use App\Http\Controllers\dashboard\CategoryController;
-use App\Http\Controllers\dashboard\SubcategoryController;
+use App\Http\Controllers\dashboard\ContactDataController;
 use App\Http\Controllers\dashboard\DashboardMainController;
+use App\Http\Controllers\dashboard\SubcategoryController;
+use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\UserShopController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
-use App\Http\Controllers\LanguageController;
-use App\Http\Middleware\custom_middleware\dashboard;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Auth::routes();
-Route::group(
-    [
-        'prefix' => LaravelLocalization::setLocale(),
-        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
-    ], function(){
 
-        Route::view('/', 'website.pages.index')->name('home');
-        Route::view('/about', 'website.pages.about')->name('about');
-        Route::view('/shop', 'website.pages.shop')->name('shop');
+Route::group([
+    'prefix' => LaravelLocalization::setLocale(),
+    'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+], function () {
+
+    // Public website routes
+    Route::view('/', 'website.pages.index')->name('home');
+    Route::view('/about', 'website.pages.about')->name('about');
+    Route::view('/shop', 'website.pages.shop')->name('shop');
+    Route::view('/cart', 'website.pages.cart')->name('cart');
+
+    // User profile and contact routes
     Route::middleware('auth')->group(function () {
         Route::get('/profile', [UserShopController::class, 'profile'])->name('profile');
         Route::post('/profile', [UserShopController::class, 'updateProfile'])->name('profile.update');
     });
-        Route::view('/contact', 'website.pages.contact')->name('contact');
-        Route::view('/cart', 'website.pages.cart')->name('cart');
 
-        Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home-auth');
+    Route::view('/contact', 'website.pages.contact')->name('contact');
+    Route::post('/contact', [UserShopController::class, 'contactForm'])->name('contact.submit')->middleware('throttle:contactForm');
 
+    // Authenticated homepage route
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home-auth');
 
-        Route::group(
-            [
-            'middleware' => ['auth', 'dashboard']], function(){
-                Route::prefix('dashboard')->group(function(){
-                    Route::get('/', [DashboardMainController::class, 'index'])->middleware(dashboard::class)->name('dashboard');
-                    // categories
-                    Route::resource('categories', CategoryController::class);
-                    Route::get('/category/delete' , [CategoryController::class , 'delete'])->name('categories.delete');
-                    Route::get('/category/restore/{id}' ,[CategoryController::class , 'restore'])->name('categories.restore');
-                    Route::delete('/category/forceDelete/{id}' , [CategoryController::class , 'forceDelete'])->name('categories.forceDelete');
-                    // sub categories
-                    Route::resource('sub-categories', SubcategoryController::class);
-                     Route::get('/subcategory/delete' , [SubcategoryController::class , 'delete'])->name('subcategories.delete');
-                     Route::get('/subcategory/restore/{id}' ,[SubcategoryController::class , 'restore'])->name('subcategories.restore');
-                     Route::delete('/subcategory/forceDelete/{id}' , [SubcategoryController::class , 'forceDelete'])->name('subcategories.forceDelete');
-                    // // products
-                    // Route::resource('products', ProductController::class);
-                    // Route::get('/product/delete' , [ProductController::class , 'delete'])->name('products.delete');
-                    // Route::get('/product/restore/{id}' ,[ProductController::class , 'restore'])->name('products.restore');
-                    // Route::delete('/product/forceDelete/{id}' , [ProductController::class , 'forceDelete'])->name('products.forceDelete');
+    // Dashboard routes
+    Route::middleware(['auth', 'dashboard'])->prefix('dashboard')->group(function () {
 
-                });
-            });
+        Route::get('/', [DashboardMainController::class, 'index'])->name('dashboard');
 
+        // Category management routes
+        Route::resource('categories', CategoryController::class);
+        Route::get('category/delete', [CategoryController::class, 'delete'])->name('categories.delete');
+        Route::get('category/restore/{id}', [CategoryController::class, 'restore'])->name('categories.restore');
+        Route::delete('category/forceDelete/{id}', [CategoryController::class, 'forceDelete'])->name('categories.forceDelete');
 
+        // Subcategory management routes
+        Route::resource('sub-categories', SubcategoryController::class);
+        Route::get('subcategory/delete', [SubcategoryController::class, 'delete'])->name('subcategories.delete');
+        Route::get('subcategory/restore/{id}', [SubcategoryController::class, 'restore'])->name('subcategories.restore');
+        Route::delete('subcategory/forceDelete/{id}', [SubcategoryController::class, 'forceDelete'])->name('subcategories.forceDelete');
+
+        Route::resource('contact-data', ContactDataController::class);
+
+        // Uncomment these if product routes are needed in the future
+        // Route::resource('products', ProductController::class);
+        // Route::get('product/delete', [ProductController::class, 'delete'])->name('products.delete');
+        // Route::get('product/restore/{id}', [ProductController::class, 'restore'])->name('products.restore');
+        // Route::delete('product/forceDelete/{id}', [ProductController::class, 'forceDelete'])->name('products.forceDelete');
     });
+});
